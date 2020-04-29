bfa5327a9604eaa0b58e2d437a50c4428d9f6a90
Simon Riezebos <simon@datagiant.org>
2020-04-29 12:14:31 +0200

    Explore suv share

--- suv/make_suv_plot.py
+++ suv/make_suv_plot.py
@@ -15,42 +15,72 @@
 
 # %%
 import requests
-from requests.utils import requote_uri, unquote
 import pandas as pd
 import plotly.express as px
 import plotly.io as pio
 
 # %%
-unquote("https://opendata.rdw.nl/api/id/m9d7-ebf2.json?$query=SELECT%20`datum_eerste_toelating`/100%20AS%20__dimension_alias__%2C%20COUNT(*)%20AS%20__measure_alias__%20WHERE%20(`voertuigsoort`%20%3D%20%27Personenauto%27)%20AND%20((`bruto_bpm`%20%3E%20%275000%27)%20AND%20`bruto_bpm`%20IS%20NOT%20NULL)%20AND%20((`massa_rijklaar`%20%3E%20%272000%27)%20OR%20`massa_rijklaar`%20IS%20NULL)%20AND%20((`aantal_zitplaatsen`%20%3E%20%273%27%20AND%20`aantal_zitplaatsen`%20%3C%20%278%27)%20AND%20`aantal_zitplaatsen`%20IS%20NOT%20NULL)%20AND%20(`inrichting`%20%3D%20%27stationwagen%27%20OR%20`inrichting`%20%3D%20%27MPV%27)%20AND%20((`vervaldatum_apk`%20%3E%20%2720200427%27)%20OR%20`vervaldatum_apk`%20IS%20NULL)%20GROUP%20BY%20`datum_eerste_toelating`/100%20ORDER%20BY%20__dimension_alias__%20DESC%20NULL%20LAST%20LIMIT%201001&$$read_from_nbe=true&$$version=2.1")
+url = """https://opendata.rdw.nl/api/id/m9d7-ebf2.json?$query=
+SELECT 
+    merk,
+    handelsbenaming,
+    COUNT(*) AS aantal
+WHERE 
+datum_eerste_toelating > '20100101'
+AND datum_eerste_toelating < '20100201'
+AND voertuigsoort = 'Personenauto'
+GROUP BY merk, handelsbenaming
+ORDER BY aantal DESC
+"""
+res = requests.get(url).json()
+df = pd.DataFrame(data=res)
+df.head(50)
 
 # %%
 url = """https://opendata.rdw.nl/api/id/m9d7-ebf2.json?$query=
 SELECT 
-    floor(`datum_eerste_toelating`/100) AS datum,
-    COUNT(*) AS aantal 
+    merk,
+    handelsbenaming,
+    COUNT(*) AS aantal
 WHERE 
-(`voertuigsoort` = 'Personenauto') 
-AND (`bruto_bpm` > '5000') 
-AND (`massa_rijklaar` > '2000') 
-AND (`aantal_zitplaatsen` between '3' and '8') 
-AND (`inrichting` = 'stationwagen' OR `inrichting` = 'MPV') 
-AND (`datum_eerste_toelating` > '20100101') 
-GROUP BY datum 
-ORDER BY datum ASC NULL LAST LIMIT 10000&$$read_from_nbe=true&$$version=2.1"""
+datum_eerste_toelating > '20100101'
+AND datum_eerste_toelating < '20100201'
+AND voertuigsoort = 'Personenauto'
+AND bruto_bpm > '5000'
+AND massa_rijklaar > '2000'
+AND aantal_zitplaatsen between '3' and '8'
+AND (inrichting = 'stationwagen' OR inrichting = 'MPV')
+GROUP BY merk, handelsbenaming
+ORDER BY aantal DESC
+"""
+res = requests.get(url).json()
+df = pd.DataFrame(data=res)
+df.head(40)
+
+# %%
+url = """https://opendata.rdw.nl/api/id/m9d7-ebf2.json?$query=
+SELECT 
+    floor(datum_eerste_toelating/100) AS datum,
+    AVG(CASE(
+        bruto_bpm > '5000' 
+        AND massa_rijklaar > '2000'
+        AND aantal_zitplaatsen between '3' and '8'
+        AND (inrichting = 'stationwagen' OR inrichting = 'MPV')
+        ,100
+        ,true
+        ,0
+    )) as suv_share
+WHERE 
+datum_eerste_toelating > '20100101'
+AND voertuigsoort = 'Personenauto'
+GROUP BY datum
+"""
 res = requests.get(url).json()
 df = pd.DataFrame(data=res)
 df.head()
+df.datum.min()
+df.datum.max()
 
 # %%
-df.datum = pd.to_datetime(df.datum,format="%Y%m")
-
-# %%
-fig = px.line(df,x="datum",y='aantal',template='plotly_white')
-
-# %%
-pio.write_html(fig,"test.html",include_plotlyjs='cdn')
-
-# %%
-fig
-
-# %%
+df.datum = pd.to_datetime(df.datum, format="%Y%m")
+px.line(df, x="datum", y="suv_share", template="plotly_white")
